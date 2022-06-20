ea9df90d0dfa9538a6de5c87a512b4637ef50e84
Devansh Choudhary <choudhary.devansh1007@gmail.com>
2022-06-21 00:40:12 +0530

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAfUpEJSImUCNN4aj4YzHC7eRNtYdpUXmI",
-    authDomain: "contact-form-ee3e7.firebaseapp.com",
-    projectId: "contact-form-ee3e7",
-    storageBucket: "contact-form-ee3e7.appspot.com",
-    messagingSenderId: "279587516172",
-    appId: "1:279587516172:web:c5bb760ce01ac2d0af6329",
-    databaseURL: "https://contact-form-ee3e7-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyBcQr-xm0gqX0C0eB025v63feoRXRyx6dM",
+  authDomain: "team-expansion-54ae7.firebaseapp.com",
+  databaseURL: "https://team-expansion-54ae7-default-rtdb.firebaseio.com",
+  projectId: "team-expansion-54ae7",
+  storageBucket: "team-expansion-54ae7.appspot.com",
+  messagingSenderId: "932038990518",
+  appId: "1:932038990518:web:02f65a0122d0ff39bfa417",
+  measurementId: "G-NCB4XT3N2C"
 };
 
 const app = initializeApp(firebaseConfig);
